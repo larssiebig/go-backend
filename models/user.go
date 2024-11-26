0e7494fa95482f111099f8203c24524b4b6624a7
larssiebig <lars.siebig.ls@gmail.com>
2024-11-26 11:17:24 +0100

    added deleteduser comment

--- models/user.go
+++ models/user.go
@@ -31,6 +31,7 @@ func CreateUser(username, password string) error {
 	return err
 }
 
+// DeletedUser from the database by ID
 func DeleteUser(userID int) error {
 	query := `DELETE FROM users WHERE id=$1`
 	_, err := utils.DB.Exec(context.Background(), query, userID)
