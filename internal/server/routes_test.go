15222e2988e403112f07bfa4713431f00d67faba
larssiebig <lars.siebig.ls@gmail.com>
2025-02-02 16:35:34 +0100

    updated initial commit

---
+++ internal/server/routes_test.go
@@ -0,0 +1,56 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TestHelloWorldHandler(t *testing.T) {
+	s := &Server{}
+	r := gin.New()
+	r.GET("/", s.HelloWorldHandler)
+	// Create a test HTTP request
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+	// Serve the HTTP request
+	r.ServeHTTP(rr, req)
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	// Check the response body
+	expected := "{\"message\":\"Hello World\"}"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
+
+func TestProfileHandler(t *testing.T) {
+	s := &Server{}
+	r := gin.New()
+	r.GET("/profile", s.ProfileHandler)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	r.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK{
+		t.Errorf("Handler returned wrong status code: got %v want %v", status, http.StatusOK)
+	}
+	
+	expected := "{\"message\":\"Welcome to your profile\"}"
+	if rr.Body.String() != expected {
+		t.Errorf("Handler returned enxpected body: got %v want %v", rr.Body.String(), expected)
+	}
+}
